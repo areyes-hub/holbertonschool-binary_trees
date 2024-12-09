73369760dabddaeb774341fd299032773164577c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-09 18:26:18 -0400

    Measure the depth of a node

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,15 +9,9 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t depth = 0;
-
 	if (!tree)
 	{
 		return (0);
 	}
-	if (tree->parent)
-	{
-		depth += 1 + binary_tree_depth(tree->parent);
-	}
-	return (depth);
+	return (1 + binary_tree_depth(tree->parent));
 }

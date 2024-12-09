55fa7213dd1eefb78d72f128a53ca651bb3d9636
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-09 16:14:32 -0400

    Use preorder traversal

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,6 +12,10 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 
 	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
 	if (parent == NULL)
 	{
 		parent = new_node;

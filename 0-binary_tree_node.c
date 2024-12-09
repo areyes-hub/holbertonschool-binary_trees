4fa77b72e9aec3c94a76834864bfabf062d1cbb7
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-09 11:58:44 -0400

    Create node

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * binary_tree_node - Creates a binary tree node
+ * @parent: A pointer to the parent node of the new node
+ * @value: The valueto put in the new node
+ * Return: A pointer to the new node on success. Else, NULL.
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = (binary_tree_t *)malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+	{
+		parent = new_node;
+	}
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	return (new_node);
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # holbertonschool-binary_trees
 Binary Trees files and tasks.
+
+# Author
+[Angel Reyes](https://www.github.com/areyes-hub)

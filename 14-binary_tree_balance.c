ac3be1c3cfe58b4752d5f5236a0527b3cf48af9a
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-12-10 11:14:11 -0400

    Measure the balance factor

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,18 +2,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * binary_tree_balance - measures the balance factor of a binary tree
- * @tree: pointer to the root node of the tree to measure the balance
- * Return: The balance factor.
+ * binary_tree_balance - A function that measures the balance factor of a
+ * binary tree.
+ * @tree: Pointer to root node to calculate balance factor.
+ * Return: The balance factor of the tree.
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left, right;
+	size_t left_height = 0, right_height = 0;
 
-	if (tree)
-	{
-		left = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-		right = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
-	}
-	return (left - right);
+	if (!tree)
+		return (0);
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+	return (left_height - right_height);
+}
+
+/**
+ * binary_tree_height - gets the height of a binary tree
+ * @tree: root node to draw height from for tree
+ * Return: size_t representing height, 0 on failure or NULL.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (!tree)
+		return (0);
+
+	left_size += 1 + binary_tree_height(tree->left);
+	right_size += 1 + binary_tree_height(tree->right);
+	if (left_size > right_size)
+		return (left_size);
+
+	return (right_size);
 }
